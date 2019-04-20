06519a975c53248f0787e4b802bcf18774901f4f
安子萱 <2669262799@qq.com>
2019-04-20 22:37:47 +0800

    update elementUI

---
+++ ElementUI/07-switch开关/01-文字秒速.vue
@@ -0,0 +1,27 @@
+使用active-text属性与inactive-text属性来设置开关的文字描述。
+<template>
+  <el-switch
+  v-model="value3"
+  active-text="按月付费"
+  inactive-text="按年付费">
+  </el-switch>
+  <el-switch
+    style="display: block"
+    v-model="value4"
+    active-color="#13ce66"
+    inactive-color="#ff4949"
+    active-text="按月付费"
+    inactive-text="按年付费">
+  </el-switch>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value3: true,
+        value4: true
+      }
+    }
+  };
+</script>
---
+++ ElementUI/22-messageBox弹框/01-确认框与action提示.vue
@@ -0,0 +1,28 @@
+调用$confirm方法即可打开消息提示，它模拟了系统的 confirm。Message Box 组件也拥有极高的定制性，我们可以传入options作为第三个参数，它是一个字面量对象。type字段表明消息类型，可以为success，error，info和warning，无效的设置将会被忽略。注意，第二个参数title必须定义为String类型，如果是Object，会被理解为options。在这里我们用了 Promise 来处理后续响应。
+<template>
+  <el-button type="text" @click="open">点击打开 Message Box</el-button>
+</template>
+
+<script>
+  export default {
+    methods: {
+      open() {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+      }
+    }
+  }
+</script>
